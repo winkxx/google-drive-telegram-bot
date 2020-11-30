0fa87cb21d0a82ece6989a8343d7d4c444abe40f
winkxx <68945988+winkxx@users.noreply.github.com>
2020-11-30 18:31:21 +0800

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -1,5 +1,5 @@
 class config:
-    BOT_TOKEN = "1402684613:AAHY8G_qN8wRFHvT0d9NWfbweZRr8zrYT6I"
+    BOT_TOKEN = "1359161992:AAFWK3WvsWGvGzgpE8vKmY9iJu3yiPiSCUw"
     APP_ID = "2948911"
     API_HASH = "d694de0c87f7bee3ecc57da8d0eda7ea"
     DATABASE_URL = "postgres://blpuicpwdoimog:e257c2e3b6effc2f37b7fa03475e03ed0f6a9367ea25a6895d6059fd8d6f5313@ec2-54-158-222-248.compute-1.amazonaws.com:5432/dbfb34cb9jqrr9"
